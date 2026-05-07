4f167f600b4abbcc5293134f3489735795286f5d
Tamila Yefimenko <tamilaedu88@gmail.com>
2026-05-07 20:37:01 +0300

    add markup to NoteList

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,5 +1,25 @@
-const NoteList = () => {
-  return <div></div>;
+import type { Note } from '../../types/note';
+import css from './NoteList.module.css';
+
+interface NoteListProps {
+  notes: Note[];
+}
+
+const NoteList = ({ notes }: NoteListProps) => {
+  return (
+    <ul className={css.list}>
+      {notes.map(note => (
+        <li className={css.listItem}>
+          <h2 className={css.title}>{note.title}</h2>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+            <button className={css.button}>Delete</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default NoteList;
