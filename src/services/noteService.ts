19817f24e7d6ff451757a094ccabf4f3c7eaecc5
Tamila Yefimenko <tamilaedu88@gmail.com>
2026-05-08 23:27:46 +0300

    update fetchNotes function

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -3,12 +3,12 @@ import type { NotesHTTPResponce, Note } from '../types/note';
 
 const token = import.meta.env.VITE_NOTEHUB_TOKEN;
 
-export const fetchNotes = async (query: string, page: number) => {
+export const fetchNotes = async (search: string, page: number) => {
   const responce = await axios.get<NotesHTTPResponce>(
     'https://notehub-public.goit.study/api/notes',
     {
       params: {
-        query,
+        search,
         page,
       },
       headers: { Authorization: `Bearer ${token}` },
