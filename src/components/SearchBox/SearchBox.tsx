207057cc8083962f4a5dd3ff3dbbb812b816c2c2
Tamila Yefimenko <tamilaedu88@gmail.com>
2026-05-08 23:28:33 +0300

    add markup to SearchBox

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,5 +1,7 @@
+import css from './SearchBox.module.css';
+
 const SearchBox = () => {
-  return <div></div>;
+  return <input className={css.input} type="text" placeholder="Search notes" />;
 };
 
 export default SearchBox;
