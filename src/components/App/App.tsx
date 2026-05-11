61ff809643ecc296fb2bdd0d0aa7044d847127ab
Tamila Yefimenko <tamilaedu88@gmail.com>
2026-05-11 21:42:05 +0300

    add search logic to App, fix issue with react versions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,16 +2,37 @@ import { useState } from 'react';
 import Pagination from '../Pagination/Pagination';
 import SearchBox from '../SearchBox/SearchBox';
 import css from './App.module.css';
+import NoteList from '../NoteList/NoteList';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { fetchNotes } from '../../services/noteService';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const totalPages = 1;
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['query', searchQuery, currentPage],
+    queryFn: () => fetchNotes(searchQuery, currentPage),
+    enabled: searchQuery !== '',
+    placeholderData: keepPreviousData,
+  });
+
+  const totalPages = data?.totalPages ?? 0;
+
+  const handleChange = (query: string) => {
+    if (!query.trim) {
+      toast.error('Please, enter your search query!');
+    }
+    setSearchQuery(query.trim().toLowerCase());
+    setCurrentPage(1);
+  };
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox />
+        <SearchBox onChange={handleChange} />
         <Pagination
           totalPages={totalPages}
           currentPage={currentPage}
@@ -19,6 +40,10 @@ function App() {
         />
         <button>Create note +</button>
       </header>
+      {data && <NoteList notes={data.notes} />}
+      {isLoading && <p>Loading</p>}
+      {isError && <p>Error</p>}
+      <Toaster />
     </div>
   );
 }
--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -9,7 +9,7 @@ const NoteList = ({ notes }: NoteListProps) => {
   return (
     <ul className={css.list}>
       {notes.map(note => (
-        <li className={css.listItem}>
+        <li className={css.listItem} key={note.id}>
           <h2 className={css.title}>{note.title}</h2>
           <p className={css.content}>{note.content}</p>
           <div className={css.footer}>
