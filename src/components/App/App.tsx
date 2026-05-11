9b57a8194f26c1f1a1cc9cb9dca6d5a7133f502d
Tamila Yefimenko <tamilaedu88@gmail.com>
2026-05-11 20:39:17 +0300

    add Pagination logic and markup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import Pagination from '../Pagination/Pagination';
 import SearchBox from '../SearchBox/SearchBox';
 import css from './App.module.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const totalPages = 1;
+
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
         <SearchBox />
-        <Pagination />
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPageChange={setCurrentPage}
+        />
         <button>Create note +</button>
       </header>
     </div>
