9b0077c21834ef4c5682344aba1f6d5801fd4a84
Tamila Yefimenko <tamilaedu88@gmail.com>
2026-05-11 21:09:13 +0300

    add search logic

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,7 +1,25 @@
+import { useDebouncedCallback } from 'use-debounce';
 import css from './SearchBox.module.css';
 
-const SearchBox = () => {
-  return <input className={css.input} type="text" placeholder="Search notes" />;
+interface SearchBoxProps {
+  onChange: (query: string) => void;
+}
+
+const SearchBox = ({ onChange }: SearchBoxProps) => {
+  const handleChange = useDebouncedCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) =>
+      onChange(event.target.value),
+    1000
+  );
+
+  return (
+    <input
+      className={css.input}
+      type="text"
+      placeholder="Search notes"
+      onChange={handleChange}
+    />
+  );
 };
 
 export default SearchBox;
