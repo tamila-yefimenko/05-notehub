a57abd4e5bdd6789e354d19c093aa34f0bfc811f
Tamila Yefimenko <tamilaedu88@gmail.com>
2026-05-15 20:01:41 +0300

    refactor createNote

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
-import type { NotesHTTPResponce, Note } from '../types/note';
+import type { NotesHTTPResponce, Note, NewNote } from '../types/note';
 
 const token = import.meta.env.VITE_NOTEHUB_TOKEN;
 
-export const fetchNotes = async (search: string, page: number, perPage:number) => {
+export const fetchNotes = async (
+  search: string,
+  page: number,
+  perPage: number
+) => {
   const responce = await axios.get<NotesHTTPResponce>(
     'https://notehub-public.goit.study/api/notes',
     {
@@ -19,7 +23,7 @@ export const fetchNotes = async (search: string, page: number, perPage:number) =
   return responce.data;
 };
 
-export const createNote = async (note: string) => {
+export const createNote = async (note: NewNote) => {
   const responce = await axios.post<Note>(
     'https://notehub-public.goit.study/api/notes',
     note,
